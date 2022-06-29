e9a35a8634d7802c95c71b040bcabcd8afa25c0d
Administrator <Administrator@SC-202106161522>
2022-06-29 11:25:57 +0900

    팀장의 권한?

--- 220629-02 hello/src/Main.java
+++ 220629-02 hello/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello 팀플 깃헙");
+		System.out.println("팀장의 권한은 뭐죠?");
 		
 		System.out.println("변경");
 		
-		System.out.println("내이름은 김상우");
+		System.out.println("내이름은 박로");
+		
+		
 	}
 }
