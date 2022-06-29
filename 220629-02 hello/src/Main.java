e636d532b4212815fc0ac1363e5343fe0adf8c14
Administrator <Administrator@SC-202106161522>
2022-06-29 11:25:18 +0900

    최진혁 ABCD로 커밋했음요,

--- 220629-02 hello/src/Main.java
+++ 220629-02 hello/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello 팀플 깃헙");
+		System.out.println("ABCD");
 		
-		System.out.println("변경");
+		System.out.println("蹂�寃�");
 		
-		System.out.println("내이름은 김상우");
+		System.out.println("�궡�씠由꾩� 源��긽�슦");
 	}
 }
